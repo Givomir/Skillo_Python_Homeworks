988baf0b0357ad35a50e3266631e3b5da8e74b56
Givomir <stanislav.peev91@gmail.com>
2023-10-11 10:01:51 +0300

    Commit Homowork 1

---
+++ Homework_1.py
@@ -0,0 +1,32 @@
+a = 9
+b = 3
+print("Problem 1")
+print(a+b, a*b, a/b, a-b)
+
+ID1 = "0112100063"
+ID2 = "0112100064"
+ID3 = "0112100065"
+ID4 = "0112100066"
+ID5 = "0112100067"
+Name1 = "Georgi Dimitrov",
+Name2 = "Ivan Kirilov",
+Name3 = "Pavel Georgiev",
+Name4 = "Krasimir Ivanov",
+Name5 = "Todor Petkov"
+
+name_id_dict = {
+    "0112100064" : "Ivan Kirilov",
+}
+print("Problem 2")
+print(name_id_dict)
+
+X = True
+Y = True
+Xor = False
+Yor = False
+
+print("Problem 3")
+print(X and Y, X or Y, not X , not Y)
+print(X and Yor, X or Yor, not X, not Yor)
+print(Xor and Y, Xor or Y, not Xor, not Y)
+print(Xor and Yor, Xor or Yor, not Xor, not Yor)
